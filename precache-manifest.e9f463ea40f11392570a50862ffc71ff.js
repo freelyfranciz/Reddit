2194f3a00aaa3140f633b340ef19208c07e9933a
Freely Francis <freelyfranciz@gmail.com>
2020-10-01 21:58:02 -0400

    initial commit

---
+++ precache-manifest.e9f463ea40f11392570a50862ffc71ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b7ff585ce98cab102d5bf9de36b6e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a683624bc78fc7ab939",
+    "url": "/static/css/main.50924595.chunk.css"
+  },
+  {
+    "revision": "4cd101dde12538c835b0",
+    "url": "/static/js/2.bdec41e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bdec41e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a683624bc78fc7ab939",
+    "url": "/static/js/main.fcb48d21.chunk.js"
+  },
+  {
+    "revision": "db6d5e1e6e357afde225",
+    "url": "/static/js/runtime-main.1fa295e1.js"
+  }
+]);
